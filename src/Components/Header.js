50e86b5d070e34947bcab308c3e2cdf620af8048
Melissa Ridente <melissaridente3e@gmail.com>
2023-04-02 18:23:36 +0200

    updating header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,41 @@
 import React from 'react'
+import {  PhoneOutlined } from '@ant-design/icons';
 import logo from '../assets/logo.png'
 import nombre from '../assets/nombre.png'
-import {PhoneOutlined} from '@ant-design/icons'
-
+import {Link} from 'react-router-dom'
 
 export const Header = () => {
+ 
   return (
-    <nav className='NavBar'>
-      <img src={logo} width={'100px'}/>
+    <>
+    
+    <div className='NavBar'>
+      <img src={logo} style={{width: '100px'}} />
       <img src={nombre} className= 'Imagen'/> 
       <div className='Phone'>
         <PhoneOutlined></PhoneOutlined>
         <p style={{margin:'10px'}}> + 34 687 589 654</p>
       </div>
-    </nav>
+    </div>
+    <div className='Links'>
+      <Link to='/' style={{margin: '20px'}}>🏠HOME</Link>
+      <Link to='/vuelos' style={{margin: '20px'}}>✈️VUELOS</Link>
+      <Link to='/cruceros' style={{margin: '20px'}}>🚢CRUCEROS</Link>
+    </div>
+    <div >
+       
+    
+       
+    </div> 
+  </>
   )
-}
+};
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
 
    
  
